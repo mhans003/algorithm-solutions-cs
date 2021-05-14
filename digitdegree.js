7d3d13d3f4371f2fa4ef811d4b241c05a012102d
mhans003 <michaeledwardhanson@gmail.com>
2021-05-14 16:29:13 -0700

    Added solution for Digit Degree

---
+++ digitdegree.js
@@ -0,0 +1,21 @@
+/*
+Let's define digit degree of some positive integer as the number of times we need to replace this number with the sum of its digits until we get to a one digit number.
+
+Given an integer, find its digit degree.
+*/
+
+function digitDegree(n) {
+    //Copy string representation of this number.
+    let currentNumber = String(n);
+    let iterations = 0;
+    while(currentNumber.length > 1) {
+        iterations++;
+        let digitArray = currentNumber.split("");
+        let sum = 0;
+        for(let i = 0; i < digitArray.length; i++) {
+            sum += Number(digitArray[i]);
+        }
+        currentNumber = String(sum);
+    }
+    return iterations;
+}
