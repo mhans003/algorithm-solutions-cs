41f5329288a7cf7b7bfd9f1926e9a04c526a7572
mhans003 <michaeledwardhanson@gmail.com>
2021-05-15 15:07:35 -0700

    Added instructions for Is Beautiful String

--- isBeautifulString.js
+++ isBeautifulString.js
@@ -1,3 +1,9 @@
+/*
+A string is said to be beautiful if each letter in the string appears at most as many times as the previous letter in the alphabet within the string; ie: b occurs no more times than a; c occurs no more times than b; etc. Note that letter a has no previous letter.
+
+Given a string, check whether it is beautiful.
+*/
+
 function isBeautifulString(inputString) {
     //Create character map using character codes as keys
     let inputArray = inputString.split("");
