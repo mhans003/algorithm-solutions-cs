7ed4fa543e193f2e3f597796fa351543b6522df6
mhans003 <michaeledwardhanson@gmail.com>
2021-05-19 05:27:07 -0700

    Added instructions for File Naming solution

--- filenaming.js
+++ filenaming.js
@@ -1,3 +1,9 @@
+/*
+You are given an array of desired filenames in the order of their creation. Since two files cannot have equal names, the one which comes later will have an addition to its name in a form of (k), where k is the smallest positive integer such that the obtained name is not used yet.
+
+Return an array of names that will be given to the files.
+*/
+
 function fileNaming(names) {
     //Store all names and see how many times they occur.
     let uniqueNames = {};
