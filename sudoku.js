9d5898ed1f704b459752bfee9479863f38cf49f1
mhans003 <michaeledwardhanson@gmail.com>
2021-05-21 16:10:57 -0700

    Added solution to Sudoku

---
+++ sudoku.js
@@ -0,0 +1,46 @@
+function sudoku(grid) {
+    let valid = true;
+    //Test the rows.
+    for(let i = 0; i < grid.length; i++) {
+        let existingNums = {};
+        for(let j = 0; j < grid[i].length; j++) {
+            //If we find a repeating number in this row, return false.
+            if(existingNums[grid[i][j]]) {
+                console.log('rows invalid');
+                valid = false;
+            }
+            existingNums[grid[i][j]] = true;
+        }
+    }
+    //Test the columns
+    for(let j = 0; j < grid.length; j++) {
+        let existingNums = {};
+        for(let i = 0; i < grid.length; i++) {
+            //If we find a repeating number in this column, return false;
+            if(existingNums[grid[i][j]]) {
+                console.log('columns invalid');
+                valid = false;
+            }
+            existingNums[grid[i][j]] = true;
+        }
+    }
+    //Test the squares
+    for(let i = 0; i < grid.length; i += 3) {
+        for(let j = 0; j < grid[i].length; j += 3) {
+            let existingNums = {};
+            //For each square, start iteration through the individual numbers
+            for(let i_prime = i; i_prime < i + 3; i_prime++) {
+                for(let j_prime = j; j_prime < j + 3; j_prime++) {
+                    //If we find a repeating number in this column, return false;
+                    if(existingNums[grid[i_prime][j_prime]]) {
+                        console.log('squares invalid');
+                        valid = false;
+                    }
+                    existingNums[grid[i_prime][j_prime]] = true;
+                }
+            }
+        }
+    }
+    
+    return valid;
+}
