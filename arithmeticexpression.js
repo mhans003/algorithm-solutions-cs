2a4e2d3bcec8cdb594aa306ecd6b312011f584ba
mhans003 <michaeledwardhanson@gmail.com>
2021-05-22 18:30:45 -0700

    Added solution for Arithmetic Expression

---
+++ arithmeticexpression.js
@@ -0,0 +1,13 @@
+//Consider an arithmetic expression of the form a#b=c. Check whether it is possible to replace # with one of the four signs: +, -, * or / to obtain a correct expression.
+
+function arithmeticExpression(a, b, c) {
+    return a + b === c ? 
+    true : 
+    a - b === c ? 
+    true : 
+    a * b === c ? 
+    true : 
+    a / b === c ? 
+    true : 
+    false;
+}
