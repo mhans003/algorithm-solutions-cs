b0b645480ca587342823193ad8bb6417d58da1f0
mhans003 <michaeledwardhanson@gmail.com>
2021-05-22 17:56:20 -0700

    Added solution for Phone Call

---
+++ phonecall.js
@@ -0,0 +1,31 @@
+/*
+Some phone usage rate may be described as follows:
+
+first minute of a call costs min1 cents,
+each minute from the 2nd up to 10th (inclusive) costs min2_10 cents
+each minute after 10th costs min11 cents.
+You have s cents on your account before the call. What is the duration of the longest call (in minutes rounded down to the nearest integer) you can have?
+*/
+
+function phoneCall(min1, min2_10, min11, s) {    
+    let centsLeft = s;
+    totalMinutes = 0;
+    
+    //Purchase first minute.
+    if(centsLeft - min1 >= 0) {
+        totalMinutes++;
+        centsLeft -= min1;
+    }
+    //Purchase minutes 2-10.
+    while(totalMinutes < 10 && centsLeft - min2_10 >= 0) {
+        totalMinutes++;
+        centsLeft -= min2_10;
+    }
+    //Purchase minutes past 10th.
+    while(totalMinutes >= 10 && centsLeft - min11 >= 0) {
+        totalMinutes++;
+        centsLeft -= min11;
+    }
+    
+    return totalMinutes;
+}
