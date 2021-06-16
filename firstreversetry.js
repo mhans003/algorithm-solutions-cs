9c5fe37be22c4632bb5947e98c6e1777d379b99d
mhans003 <michaeledwardhanson@gmail.com>
2021-06-16 05:01:15 -0700

    Added solution to First Reverse Try

---
+++ firstreversetry.js
@@ -0,0 +1,10 @@
+/*
+Reversing an array can be a tough task, especially for a novice programmer. Mary just started coding, so she would like to start with something basic at first. Instead of reversing the array entirely, she wants to swap just its first and last elements.
+
+Given an array arr, swap its first and last elements and return the resulting array.
+*/
+
+function firstReverseTry(arr) {
+    if(arr.length) [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
+    return arr;
+}
