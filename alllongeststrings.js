bba8f54ac504700ea62576878715ef01c939c71a
mhans003 <michaeledwardhanson@gmail.com>
2021-07-02 12:11:47 -0700

    Improved solution for All Longest Strings

--- alllongeststrings.js
+++ alllongeststrings.js
@@ -3,19 +3,6 @@ Given an array of strings, return another array containing all of its longest st
 */
 
 function allLongestStrings(inputArray) {
-    var newArray = []; 
-    let longestLength = 0; 
-    //Get the longest length
-    inputArray.forEach((str) => {
-        if(str.length > longestLength) {
-            longestLength = str.length; 
-        }
-    }); 
-    //Fill with the new items 
-    inputArray.forEach((str) => {
-        if(str.length === longestLength) {
-            newArray.push(str); 
-        }
-    }); 
-    return newArray; 
+    const longestLength = Math.max(...inputArray.map(element => element.length));
+    return inputArray.filter(element => element.length === longestLength);
 }
