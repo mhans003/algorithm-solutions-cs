f260a2979a8babca4f6fde3890f9df5a8721c577
mhans003 <michaeledwardhanson@gmail.com>
2021-08-13 15:38:49 -0700

    Updated refactor for Valid Time

--- validtime.js
+++ validtime.js
@@ -1,11 +1,23 @@
-//Check if the given string is a correct time representation of the 24-hour clock.
+/*
+Check if the given string is a correct time representation of the 24-hour clock.
+
+Example
+
+For time = "13:58", the output should be
+validTime(time) = true;
+For time = "25:51", the output should be
+validTime(time) = false;
+For time = "02:76", the output should be
+validTime(time) = false.
+*/
 
 function validTime(time) {
-    //Extract the hour and minute.
-    let hour = time.substring(0, time.indexOf(":"));
-    let minute = time.substring(time.indexOf(":") + 1);
-    //Make sure these values are within the correct ranges.
-    if(!(Math.abs(Number(hour)) <= 23 && Math.abs(Number(minute)) <= 59)) return false;
-    
-    return true;
+    //Create number versions of this string.
+    let values = time.split(":").map(str => Number(str));
+    //Check if the values (hour and minutes) are within correct range.
+    return values[0] < 24 && values[0] >= 0 ? 
+    values[1] < 60 && values[1] >= 0 ?
+    true :
+    false :
+    false;
 }
