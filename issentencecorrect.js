e6c233915bef9452640de76f668ee51375b4c056
mhans003 <michaeledwardhanson@gmail.com>
2021-08-23 12:19:49 -0700

    Added instructions for Is Sentence Correct

--- issentencecorrect.js
+++ issentencecorrect.js
@@ -1,3 +1,12 @@
+/*
+A sentence is considered correct if:
+
+it starts with a capital letter;
+it ends with a full stop, question mark or exclamation point ('.', '?' or '!');
+full stops, question marks and exclamation points don't appear anywhere else in the sentence.
+Given a sentence, return true if it is correct and false otherwise.
+*/
+
 function isSentenceCorrect(sentence) {
     //^[A-Z] searches for a capital letter at the beginning.
     //[^.?!]* searches for 0+ instances of any character that is NOT (^) final punctuation.
