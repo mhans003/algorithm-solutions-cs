c49838cae113e07037f8a19eecef922d911d32e6
mhans003 <michaeledwardhanson@gmail.com>
2021-08-25 12:55:21 -0700

    Added solution to Is Subsequence

---
+++ issubsequence.js
@@ -0,0 +1,14 @@
+//Given a string s, determine if it is a subsequence of a given string t.
+
+function isSubsequence(t, s) {
+    var pattern = "";
+    //For each letter in the string s (used to build the pattern to apply to string t), incrementally construct with the current element.
+    //Since we are looking for that particular character, use [].
+    //.* together looks for that character, then at some later point, the following character.
+    for (var i = 0; i < s.length; i++) {
+      pattern += `[${s[i]}].*`;
+    }
+    var re = new RegExp(pattern);
+    return re.test(t);
+  }
+  
