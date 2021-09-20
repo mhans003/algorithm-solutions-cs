870d08b4c86cbfac2656efb1deff08f082a67784
mhans003 <michaeledwardhanson@gmail.com>
2021-09-20 10:36:48 -0700

    Added instructions to Sudoku

--- sudoku.js
+++ sudoku.js
@@ -1,5 +1,7 @@
 /*
-Added instructions to Sudoku
+Sudoku is a number-placement puzzle. The objective is to fill a 9 × 9 grid with digits so that each column, each row, and each of the nine 3 × 3 sub-grids that compose the grid contains all of the digits from 1 to 9.
+
+This algorithm should check if the given grid of numbers represents a correct solution to Sudoku.
 */
 
 function sudoku(grid) {
