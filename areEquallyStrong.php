021cc669a21e414356f867c41595b21e19c5b68e
mhans003 <michaeledwardhanson@gmail.com>
2021-11-26 12:51:35 -0800

    Added PHP solution for areEquallyStrong

---
+++ areEquallyStrong.php
@@ -0,0 +1,5 @@
+function solution($yourLeft, $yourRight, $friendsLeft, $friendsRight) {
+    return $yourLeft + $yourRight === $friendsLeft + $friendsRight &&
+    ($yourLeft === $friendsLeft || $yourRight === $friendsRight || $yourLeft === $friendsRight ||
+    $yourRight === $friendsLeft);
+}
