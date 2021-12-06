8a90cd80d05e7953e11d726f9b3c3f0c19a386fb
mhans003 <michaeledwardhanson@gmail.com>
2021-12-06 06:33:29 -0800

    Added Python solution to arithmeticExpression

---
+++ arithmeticExpression.py
@@ -0,0 +1,10 @@
+def solution(a, b, c):
+    if(a + b == c):
+        return True
+    elif(a - b == c):
+        return True 
+    elif(a * b == c):
+        return True
+    elif(a / b == c):
+        return True
+    return False
