d90fa553fc5486fbd0315cce5b5b33242151913b
mhans003 <michaeledwardhanson@gmail.com>
2022-02-06 11:23:17 -0800

    Added solution to rotateString

---
+++ rotateString.js
@@ -0,0 +1,12 @@
+/*
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+*/
+
+var rotateString = function(s, goal) {
+    if((s + s).includes(goal) && goal.length >= s.length) return true;
+    return false;
+};
